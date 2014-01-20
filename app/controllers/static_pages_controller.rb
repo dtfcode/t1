ff13aa251156021fee8a293dc02bf29f71851ab5
DT Freeman <dtfcode@gmail.com>
2014-01-20 07:59:11 -0500

    Revert 34bc197..5876d7a
    This rolls back to commit 34bc197751d100741a536db4b78fa95dcc6114c8.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@ class StaticPagesController < ApplicationController
 
   def home
     @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def help
